41d94426e3e08039a39516137d5d24ad758fc81c
thisisblain <thisisblain@gmail.com>
2024-08-08 01:55:11 +0200

    first-commit

---
+++ JavaScript/question2.js
@@ -0,0 +1,11 @@
+var date = new
+date(document.lastModified);
+document.write(date); 
+
+
+// function dateUpdate() {  
+//     var valueReturned = document.getElementById("dateInput").value;
+ 
+//  { alert("This page was last updated on: " + valueReturned );
+// }
+// }
